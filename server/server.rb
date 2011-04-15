19d409aee1c1eb3a26530dc0ad4e98b7d1484c90
sky-actinium <devnull@localhost>
2011-04-16 07:49:28 +0900

    Add api server supports /redirct, /current and /users

--- server/server.rb
+++ server/server.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/ruby
 
 require 'uri'
+require 'webrick'
 require 'rubygems'
 require 'em-websocket'
 
 PORT=8088
+APIPORT=8089
 
 connections = []
 url = "http://yahoo.co.jp"
@@ -14,6 +16,26 @@ Thread.new do
 		sleep 60
 		puts "Current users: #{connections.length}"
 	end
+end
+
+Thread.new do
+	srv = WEBrick::HTTPServer.new({:BindAddress => '0.0.0.0', :Port => APIPORT})
+	srv.mount_proc("/") do |req, res|
+		res.body = "use /redirect, /current or /users"
+	end
+	srv.mount_proc("/redirect") do |req, res|
+		res.body = "<html><body><a href=\"#{url}\">#{url}</a></body></html>"
+		res["location"] = url
+		res.status = 302
+	end
+	srv.mount_proc("/current") do |req, res|
+		res.body = "<html><body><a href=\"#{url}\">#{url}</a></body></html>"
+		res["Content-type"] = "text/html"
+	end
+	srv.mount_proc("/users") do |req, res|
+		res.body = connections.length.to_s
+	end
+	srv.start
 end
 
 EventMachine::WebSocket.start(:host=>"0.0.0.0", :port=>PORT) do |ws|
